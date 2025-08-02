f3d93963f369ee89a25f8fcfb962084e271f16df
minhnhat2048 <nhatle2048@gmail.com>
2025-08-02 15:40:56 +0700

    refactor: product schema to show full attributes

--- backend/app/repository/product_repository.py
+++ backend/app/repository/product_repository.py
@@ -1,5 +1,8 @@
 from sqlalchemy.orm import Session
 from app.model.product import Product
+from app.schema.product_schema import ProductImageResponse, ProductResponse
 
 def get_active_products(db: Session):
-    return db.query(Product).filter(Product.is_activated == True).all()
+    products = db.query(Product).filter(Product.is_activated == True).all()
+
+    return products
